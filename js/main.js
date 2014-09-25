61d352f252a2b5f96aad34f1b8c985ee97058965
VitBuk <vitbuk@gmail.com>
2014-09-25 18:03:30 +0300

    Ровно тот же код в main.js почему-то работает по-другому О_О

--- js/main.js
+++ js/main.js
@@ -15,8 +15,7 @@
         var playerList = document.getElementById("playerList");
 
         var playerColumn = "<div class=playerColumn id="+playerColumnId+">";
-        //onclick='enterName("+playerId+")
-        var playerNameDiv = "<div class=nameData id= "+playerNameId+"> "+playerNameId+" </div>";
+        var playerNameDiv = "<div class=nameData id= "+playerNameId+" onclick='enterName("+playerId+")'> "+playerNameId+" </div>";
         var playerScoreDiv = "<div class=scoreData id="+playerScoreId+" onclick='enterScore("+playerId+")'>0</div>";
         var scoreHistoryDiv = "<div id="+scoreHistoryId+"> </div>";
         //button массив через цикл?_?
@@ -34,11 +33,9 @@
 
     function changeScore (id, change) {
         var playerScoreId = "Score" + id;
-        var inputTextId = "InputScore" +id;
+        var inputScoreId = "InputScore" +id;
         var playerScoreValue = document.getElementById(playerScoreId);
-        var inputText = document.getElementById(inputTextId);
-
-        console.log ("parseINt " + playerScoreValue.innerHTML);
+        var inputText = document.getElementById(inputScoreId);
 
         if (inputText == null) {
             var scoreInt = parseInt(playerScoreValue.innerHTML);
@@ -54,9 +51,6 @@
     function changeScoreHistory (id, score, change) {
         var scoreHistoryId = "ScoreHistory" + id;
         var playerScoreHistory = document.getElementById(scoreHistoryId);
-
-        console.log (score);
-        console.log (change);
 
         if (change>0) {
             playerScoreHistory.innerHTML = "<div class=scoreHistoryData id="+scoreHistoryId+"> "+score+"<span class=scoreHistoryIncrease> +"+change+"</span></div>" + playerScoreHistory.innerHTML;
@@ -76,21 +70,31 @@
     }
 
     function enterScore (id) {
-        var inputTextId= "InputScore" + id;
+        var inputScoreId= "InputScore" + id;
         var playerScoreId = "Score" + id;
         var playerScoreValue = document.getElementById(playerScoreId);
         var scoreInt = parseInt(playerScoreValue.innerHTML);
 
-        if (document.getElementById(inputTextId) == null) {
-            playerScoreValue.innerHTML = "<input id="+inputTextId+" value="+scoreInt+" type='text' onkeypress='applyScore(event, "+scoreInt+", "+id+")' onblur='applyScore(event, "+scoreInt+", "+id+")'/>";
+        if (document.getElementById(inputScoreId) == null) {
+            playerScoreValue.innerHTML = "<input id="+inputScoreId+" value="+scoreInt+" type='text' onkeypress='applyScore(event, "+scoreInt+", "+id+")' onblur='applyScore(event, "+scoreInt+", "+id+")'/>";
         }
     }
 
+    /*   function applyScore (event, id) {
+     var inputTextId = "InputScore" + id;
+     var inputText = document.getElementById(inputTextId);
+
+     if (event.type == 'blur' || event.keyCode == 13) {
+     changeScore(id, inputText.value);
+     }
+     }
+     */
+
     function applyScore (event, score, id) {
         var playerScoreId = "Score" +id;
-        var inputScoreTxtFieldId = "InputScore" +id;
+        var inputScoreId = "InputScore" +id;
         var playerScoreValue = document.getElementById(playerScoreId);
-        var inputText = document.getElementById(inputScoreTxtFieldId);
+        var inputText = document.getElementById(inputScoreId);
 
         if (event.type == 'blur' || event.keyCode == 13) {
             if (!parseInt(inputText.value)) {
@@ -100,6 +104,45 @@
             else {
                 playerScoreValue.innerHTML = inputText.value;
                 changeScoreHistory(id, score, (inputText.value - score));
+            }
+        }
+    }
+
+    function enterName (id) {
+        var playerNameId = "Player" +id;
+        var playerName = document.getElementById(playerNameId);
+        var inputNameId = "InputName" +id;
+        var inputName = document.getElementById(inputNameId);
+        var playerOldName;
+
+        console.log ("playerOldName before if " + playerOldName);
+        var playerShowName = playerName.innerHTML;
+
+        if (inputName == null) {
+            playerOldName = playerName.innerHTML;
+            console.log ("playerOldName in if " + playerOldName);
+            playerName.innerHTML = "<input id=" + inputNameId + " value=" + playerShowName + " type'text' onkeypress='applyName(event, " + playerOldName + ", " + id + ")' onblur='applyName(event, " + playerOldName + ", " + id + ")'/>";
+            playerOldName = playerName.innerHTML;
+        }
+        else {
+            playerOldName = inputName.value;
+            console.log ("playerOldName after if " + playerOldName);
+        }
+    }
+
+    function applyName (event, name, id) {
+        var playerNameId = "Player" +id;
+        var inputNameId = "InputName" +id;
+        var playerName = document.getElementById(playerNameId);
+        var inputName = document.getElementById(inputNameId);
+
+        console.log ("Параметр name в applyName равен " + name);
+        if (event.type == 'blur' || event.keyCode == 13){
+            if (inputName.value == "") {
+                playerName.innerHTML = name;
+            }
+            else {
+                playerName.innerHTML = inputName.value;
             }
         }
     }
